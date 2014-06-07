d99c4bbd688a062c583d68884ec660b25a4646ae
Alex Kalderimis <alexkalderimis@gmail.com>
2014-06-07 22:40:07 +0100

    Allow multiple origins, keyed by hash.

--- config/demo.js
+++ config/demo.js
@@ -1,7 +1,29 @@
 define([], function () {
+  'use strict';
+
+  // Drive client ids are bound to defined origins. Make sure you use the right
+  // client for the right origin.
+  var driveClients = {};
+  driveClients[-1160247359] =
+    "325597969559-0h7jf8u9bsnb96q2uji5ee1r74vrngsu.apps.googleusercontent.com";
+
+  var origin = 0;
+  if (typeof window !== 'undefined') {
+    origin = hashString(window.location.origin);
+  }
+
   return {
-    // This drive client only works on a specific host address. It is thus
-    // useless to you! Ask Alex for the host name if you need to test.
-    driveClientId: "325597969559-0h7jf8u9bsnb96q2uji5ee1r74vrngsu.apps.googleusercontent.com"
+    driveClientId: driveClients[origin]
   };
+
+  // Simple one way hash function so we don't have to expose
+  // the origins in source control.
+  function buildHash (hash, char) {
+    hash = ((hash << 5) - hash) + char.charCodeAt(0);
+    return hash & hash;
+  }
+  function hashString (s) {
+    return s.split('').reduce(buildHash, 0);
+  }
 });
+
